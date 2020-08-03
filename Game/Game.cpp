8704faba64af6f1289b2b552d62e5c513cabbd2b
Xi-Xi <358xixi@gmail.com>
2020-08-03 11:19:48 -0600

    Set up finished

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,20 +1,27 @@
-// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <SDL.h>
 
-int main()
+int main(int, char**)
 {
-    std::cout << "Hello World!\n";
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("GAT150", 100, 100, 800, 600, SDL_WINDOW_SHOWN); 
+    if (window == nullptr) 
+    { 
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl; 
+        SDL_Quit(); 
+        return 1; 
+    }
+
+    //wait for keyboard enter to exit
+    std::getchar();
+
+    SDL_Quit();
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
